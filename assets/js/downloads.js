900bc84320af45db81f794a5f867a9c3254b99b1
Shan <47271333+shanimal08@users.noreply.github.com>
2020-06-26 08:53:14 +0200

    Getting the binaries from S3 & setting latests (#20)
    * Getting the 'Libraries' binaries from S3
    * Getting latest for everything but libwallet
    * Checking for checksums
    * Added cache url to prod config
    * Setting past versions for all OSs

--- assets/js/downloads.js
+++ assets/js/downloads.js
@@ -37,6 +37,95 @@ function removeClass(elems) {
 }
 
 jQuery(document).ready(function($) {
+  getS3Data()
+  //get data
+  function getS3Data() {
+    $.ajax({
+      url: Tari.s3BucketURL,
+      headers: { "Access-Control-Allow-Origin": "*" },
+      success: function(res) {
+        groupDataByOs(res.files);
+        setLatest(res.latest);
+      }
+    });
+  }
+
+  function groupDataByOs(data) {
+    let linux = [];
+    let windows =  [];
+    let osx = [];
+    let libWallet = [];
+    let sortedFiles = {}
+
+    data.forEach((file)=> {
+      const type = file.path.split('/')[0];
+      switch (type) {
+        case 'linux':
+          linux.push(file);
+          break;
+        case 'windows':
+          windows.push(file)
+          break;
+        case 'osx':
+          osx.push(file);
+          break;
+        default:
+         libWallet.push(file);
+      }
+       sortedFiles = {
+        linux, windows, osx, libWallet
+      }
+    })
+    Object.keys(sortedFiles).forEach((os)=>{
+      renderBinaries(sortedFiles[os], os)
+    })
+  }
+
+  function renderBinaries(data, os) {
+    let binContainer = document.getElementById(`${os}BinID`);
+    const dateOptions = { weekday: 'long', month: 'short', day: 'numeric' };
+
+    binContainer.innerHTML = data.map(( binary, index) => {
+      const lastMod = new Date(binary.lastModified);
+      const formattedDate = lastMod.toLocaleString(undefined, dateOptions);
+      const formattedTime = lastMod.getHours() + ':' + lastMod.getMinutes();
+      const altClass = index % 2 ? '' : 'alt-colour'
+      const path = binary.path.split("/").pop();
+
+        return (
+           `<div class="bin-row ${altClass}">
+              <div class="bin-row-item bin-left" scope="row">
+                <a href="${binary.url}">
+                  ${path}
+                </a>
+                </div>
+              <div class="bin-row-item bin-right">${formattedDate} at ${formattedTime}</div>
+            </div>`
+        )
+    }).join('')
+  }
+
+  function setLatest(data) {
+    Object.keys(data).forEach((os) => {
+      if (os !== 'libwallet'){
+
+        let btn = document.getElementById(`${os}DL`);
+        let checkSumDiv = document.getElementById(`${os}CSID`);
+
+        let sha256 = "";
+        let checksum = data[os].sha256;
+
+        if (checksum) {
+          sha256 = checksum.split(" ")[0];
+        }
+
+        btn.href = data[os].url;
+
+        checkSumDiv.innerHTML = checksum ? `SHA256: ${sha256}` : "";
+      }
+    })
+  }
+
   function setInitialActive(os) {
     switch (os) {
       case "Mac OS":
